03a3a0aac8d017f12fd099187858f2491a9476b1
孤桜懶契 <2324298849@qq.com>
2022-04-17 22:37:11 +0800

    Add files via upload

---
+++ config.js
@@ -0,0 +1,41 @@
+// 配置
+window.Config = {
+
+  // 站点名
+  SiteName: 'Server Status',
+
+  // 站点链接
+  SiteUrl: '/',
+
+  // UptimeRobot Api 域名
+  // 只需填写域名部分，默认为官网域名
+  // 因官网 API 时不时的会 CROS 报错，可填自定义反代域名
+  // 详见 https://github.com/yb/uptime-status/ 说明
+  ApiDomain: 'api.uptimerobot.com',
+
+  // UptimeRobot Api Keys
+  // 支持 Monitor-Specific 和 Read-Only 两只 Api Key
+  ApiKeys: [
+    'ur1702667-ac013a69cbf11b0c81c1a632',
+  ],
+
+  // 是否显示监测站点的链接
+  ShowLink: true,
+
+  // 日志天数
+  // 虽然免费版说仅保存60天日志，但测试好像API可以获取90天的
+  // 不过时间不要设置太长，容易卡，接口请求也容易失败
+  CountDays: 60,
+
+  // 导航栏菜单
+  Navi: [
+    {
+      text: '星典文库',
+      url: 'https://www.starpoc.vip/'
+    },
+    {
+      text: '星轨武器漏洞库职业版查询平台',
+      url: 'https://www.exppoc.org/'
+    }
+  ]
+};
